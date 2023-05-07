b1a59cb1d3c781c9acac66c06608e3995b1f3c96
Peyton8888 <131896853+Peyton8888@users.noreply.github.com>
2023-05-07 11:03:21 -0500

    eslint en js

---
+++ tereas/tarea10/introduccion strings.js
@@ -0,0 +1,32 @@
+let letra_s="texto con comillas simples";
+let letra_d="texto con comillas simples";
+
+console.log(letra_d);
+console.log(letra_s);
+
+
+let letra1 ="texto \"doble\"";
+let letra2 ="texto con comilla 'simple' dentro";
+let letra3 ="texto con comilla \"doble\" dentro";
+let letra4 ="texto con comilla 'simple' dentro";
+
+console.log(letra1);
+console.log(letra2);
+console.log(letra3);
+
+// strings con backticks
+let nombre ="santiago";
+let apellido ="matias";
+
+console.log(`nombre es ${nombre} su apellido es ${apellido}`);
+
+// strings con backticks adentro
+
+let nombre1 ="santiago";
+let apellido1 =`${nombre1} matias`;
+
+console.log(apellido1);
+
+//plantilla html
+
+
---
+++ z12-eslint/eslinnpm/index.js
@@ -0,0 +1,32 @@
+// instalar ESLint (npm i -D eslint)
+
+//ejecutar npm init @eslint/config
+// agg  "no-unused-vars":"off"
+
+const hola ="hola ,undo";
+
+
+const vars ="hola";
+
+let var2 = "que tal";
+/*eslint-disable*/ //desbilita el eslint
+                              var title= 'XD'
+/*eslint-enable*/
+const a = "hola";
+
+//desactiva la linea
+   const xd = '8888'  // eslint-disable-line 
+
+// eslint-disable-next-line indent
+      const xd2 = "888";
+//solo el indentado
+
+
+//para mostrar error por console poner  "no-console":"error" en el json
+console.log(xd);
+console.log(xd2);
+
+
+//mostrar errores en eslint "lint": "eslint ." pponer en script de json ejecutar npm run lint
+//corregir errores con eslint "lint-fix":"eslint --fix ."
+
